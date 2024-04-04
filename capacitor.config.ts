c12286689c0e01b57280809472eb64091d722bf7
GuiBaioni <guilhermebaioni35@gmail.com>
2024-04-04 17:38:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Peso/Altura',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
